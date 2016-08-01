5cf4d28cee39e223684efd7e0bfa2c957855dafb
one <onedv>
2016-08-01 18:05:04 +0200

    temp

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate caesar;
 
 use caesar::{TlsTcpListener, TlsTcpStream};
 use std::thread;
+use std::io::prelude::*;
 
 fn open_socket() {
     let key_path = "/home/one/missions/burp.key.pem";
@@ -9,8 +10,15 @@ fn open_socket() {
 
     let listener = TlsTcpListener::bind("127.0.0.1:8080", key_path, cert_path).unwrap();
 
-    fn handle_client(stream: TlsTcpStream) {
+    fn handle_client(mut stream: TlsTcpStream) {
         println!("Connection established {:?}", stream);
+        let mut incoming_data = String::new();
+        let mut buffer = [0; 5];
+        let mut handle = stream.take(5);
+        handle.read(&mut buffer);
+        println!("{:?}", buffer);
+        // stream.read_to_string(&mut incoming_data).unwrap();
+        // println!("{}", incoming_data);
     }
 
     for stream in listener.incoming() {
@@ -24,6 +32,7 @@ fn open_socket() {
         }
     }
 }
+
 fn main() {
     println!("Starting to listen");
     open_socket()
